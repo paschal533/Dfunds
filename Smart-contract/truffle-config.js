a658417234fdb82f0229a2ec7a3acbb211beb8d5
unknown <okwuosahpaschal@gmail.com>
2022-05-16 01:32:06 -0700

    config commit

--- Smart-contract/truffle-config.js
+++ Smart-contract/truffle-config.js
@@ -19,7 +19,7 @@ module.exports = {
       host: "test.confluxrpc.com",     // Conflux provides public RPC services for testnet
       port: 80,
       network_id: 1,       // Conflux testnet networkId is 1
-      privateKeys: ["9d2c1dc41f209792f8af782f1afcaa2bfc54dd2d67a40722143cd5b36224ffab"]  //  Adding the account private key used for sending transactions
+      privateKeys: [privateKey]  //  Adding the account private key used for sending transactions
     },
   }
 };
