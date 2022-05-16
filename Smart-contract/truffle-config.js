024cbbca83b09741d4e50982e922e4e77443101a
unknown <okwuosahpaschal@gmail.com>
2022-05-16 14:09:56 -0700

    config commit

--- Smart-contract/truffle-config.js
+++ Smart-contract/truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  // See<http://truffleframework.com/docs/advanced/configuration>
+  // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   compilers: {
     solc: {
