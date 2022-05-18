1243ba5f3454a399307e10d16610c2ff277af513
unknown <okwuosahpaschal@gmail.com>
2022-05-18 15:26:44 -0700

    update commit

--- frontend/context/contextProvider.jsx
+++ frontend/context/contextProvider.jsx
@@ -50,7 +50,7 @@ export const ContextProvider = ({ children }) => {
     const init = async () => {
       try{
         const acct = cfx.wallet.addPrivateKey('0xf507bf529f870fff107fee93220a7f0516d90914c3510d53ac08e8b723c64f0a')
-        //const contract = cfx.Contract({ abi: FundraiserFactor.abi, bytecode: FundraiserFactor.bytecode }
+        //const contract = cfx.Contract({ abi: FundraiserFactor.abi, bytecode: FundraiserFactor.bytecode })
         //const txReceipt = await contract.constructor().sendTransaction({ from: acct }).executed()
         //console.log(txReceipt);
         const contrac = await cfx.Contract({ abi: FundraiserFactor.abi, address: "cfxtest:aca855fctap4ptfyn0aak58a9t9279pjfeb0ymf6vk" })
@@ -135,7 +135,7 @@ export const ContextProvider = ({ children }) => {
   const getAfundraiser = async (fund) => {
     setModalLoading(true)
     try {
-      const acct = cfx.wallet.addPrivateKey(process.env.PrivateKey)
+      const acct = cfx.wallet.addPrivateKey('0xf507bf529f870fff107fee93220a7f0516d90914c3510d53ac08e8b723c64f0a')
       const instance = await cfx.Contract({ abi: FundraiserContract.abi, address: fund })
       setFactoryContract(instance)
 
@@ -183,11 +183,12 @@ export const ContextProvider = ({ children }) => {
   const submitFunds = async () => {
     const cfxRate = exchangeRate
     const cfxTotal = donationAmount / cfxRate
-    console.log(factoryContract)
 
     try {
+      const acct = cfx.wallet.addPrivateKey('0xf507bf529f870fff107fee93220a7f0516d90914c3510d53ac08e8b723c64f0a')
       const donation =  Drip.fromCFX(cfxTotal).toString()
-
+      console.log(acct)
+      
       const tx = await factoryContract.donate().sendTransaction({
         from: currentAccount,
         value: donation,
